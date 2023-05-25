ae2106157fe26db300869ca00ed1f27b277f1dab
SHArtyom <sartiom1@gmail.com>
2023-05-26 00:16:08 +0300

    Template inheritance added

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
+    return render(request, 'article/index.html', context={
+        'app_name': 'Articles',
     })
 
 
